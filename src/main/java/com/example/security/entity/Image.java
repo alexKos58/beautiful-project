57d10583ee2caf3761f45eadcd8ae34690185332
alexKos58 <alex_kostyushin@mail.ru>
2023-11-01 11:38:27 +0300

    Added entities and enums

---
+++ src/main/java/com/example/security/entity/Image.java
@@ -0,0 +1,32 @@
+package com.example.security.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@RequiredArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "image")
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @Column(name = "file_name")
+    private String fileName;
+
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    private Product product;
+}
